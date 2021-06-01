7b05cf402f3eb979fbce87e4abe70d58bc7f191a
Nikita Zhiltsov <nikita.zhiltsov@gmail.com>
2021-05-31 20:14:59 -0700

    Refactor beam pipeline; add data transfer to GCS in flow as workaround

--- selector-standardization-beam/setup.py
+++ selector-standardization-beam/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='selector-standardization-beam',
-    version='0.1.0',
+    version='0.2.0',
     description='Data Standardization pipeline in Apache Beam for Selector project',
     long_description=open("README.md", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
--- selector-standardization-flow/src/org/codeforrussia/selector/pipeline/flow.py
+++ selector-standardization-flow/src/org/codeforrussia/selector/pipeline/flow.py
@@ -5,14 +5,20 @@ from airflow.providers.apache.beam.operators.beam import (
 )
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 
+AIRFLOW_HOME_DIR = "/opt/airflow"
+# Working dirs
+STANDARDIZED_DATA_FOLDER = "standardized-election-data"
+OUTPUT_LOCAL_DATA_PATH = f"{AIRFLOW_HOME_DIR}/{STANDARDIZED_DATA_FOLDER}"
+
+# Google cloud storage parameters
+GCS_BUCKET = "codeforrussia-selector"
+GCS_INPUT_DATA_PATH = f"gs://{GCS_BUCKET}/shpilkin_dumps/*"
+GCS_INPUT_DUMP_FORMAT = "SHPILKIN"
+GCS_CREDENTIALS_FILE = f"{AIRFLOW_HOME_DIR}/google-configs/carbide-program-314404-b1f3be733966.json"
+
 default_args = {
     'owner': 'airflow',
-    'input_data_path': "gs://codeforrussia-selector/shpilkin_dumps/state_duma_shpilkin_dump.21-09-2020.jsonl",
-    'input_data_format': "SHPILKIN",
-    'output_cloud_data_path': "gs://codeforrussia-selector/standardized-election-data"
 }
-
-
 
 with models.DAG(
         "selector_standardize_electoral_data",
@@ -22,19 +28,29 @@ with models.DAG(
         tags=['selector'],
 ) as dag_native_python:
 
-    start_standardize_pipeline_local_direct_runner = BeamRunPythonPipelineOperator(
-        task_id="start_standardize_pipeline_direct_runner",
+    standardizing_pipeline = BeamRunPythonPipelineOperator(
+        task_id="standardizing_pipeline",
         py_file='org.codeforrussia.selector.beam.pipeline',
         py_options=['-m'],
         pipeline_options={
-                          "input": default_args["input_data_path"],
-                          "input-data-format": default_args["input_data_format"],
-                          "output": default_args["output_cloud_data_path"],
+                          "input": GCS_INPUT_DATA_PATH,
+                          "input-data-format": GCS_INPUT_DUMP_FORMAT,
+                          "output": OUTPUT_LOCAL_DATA_PATH,
+                          "google-application-credentials": GCS_CREDENTIALS_FILE,
                           },
         py_requirements=[
-            "apache-beam[gcp]==2.26.0",
+            "apache-beam[gcp]==2.29.0",
             "selector-standardizers>=0.1.0",
-            "selector-standardization-beam>=0.1.0"],
+            "selector-standardization-beam>=0.2.0",
+        ],
         py_interpreter='python3',
         py_system_site_packages=False,
     )
+
+    upload_to_gcs = LocalFilesystemToGCSOperator(
+        task_id="upload_standardized_data_to_gcs",
+        src=f"{OUTPUT_LOCAL_DATA_PATH}/*/*",
+        bucket="codeforrussia-selector",
+        dst=f"{STANDARDIZED_DATA_FOLDER}/",
+    )
+
